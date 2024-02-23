02f8a27258304ea63e2d43a06749aa878092a575
DannyGilligan <gilligan.danny@gmail.com>
2024-02-23 14:22:51 +0000

    Fix location of index.html file

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
-    """
-    This view will render the index page
-    """
+    """ A view to return the index page """
+
     return render(request, 'home/index.html')
