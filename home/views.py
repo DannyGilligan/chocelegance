069f8511546010f10430bcf1af77ffb8fe00677e
DannyGilligan <gilligan.danny@gmail.com>
2024-02-23 14:00:15 +0000

    Add index view to views.py

--- home/views.py
+++ home/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
 
+# Create your views here.
+def index(request):
+    """
+    This view will render the index page
+    """
+    return render(request, 'home/index.html')
