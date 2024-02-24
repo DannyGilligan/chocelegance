f9eb85acb7a0fb9e1d3de13613f96bba7b1296e5
DannyGilligan <gilligan.danny@gmail.com>
2024-02-24 12:57:13 +0000

    Add product model to models.py

--- products/models.py
+++ products/models.py
@@ -14,4 +14,15 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) # Optional
+    sku = models.CharField(max_length=254, null=True, blank=True) # Optional
+    name = models.CharField(max_length=254) # Required
+    description = models.TextField() # Required
+    price = models.DecimalField(max_digits=6, decimal_places=2) # Required
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) # Optional
+    image_url = models.URLField(max_length=1024, null=True, blank=True) # Optional
+    image = models.ImageField(null=True, blank=True) # Optional
 
+    def __str__(self):
+        return self.name
