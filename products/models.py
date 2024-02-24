64e8bf6612fb1a0d41e5696a1401f80fdd759d75
DannyGilligan <gilligan.danny@gmail.com>
2024-02-24 12:56:53 +0000

    Add Category model to models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254) # Required
+    friendly_name = models.CharField(max_length=254, null=True, blank=True) # Optional
+
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+
