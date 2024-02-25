39920d61d42b700de7df2165f5651f606d216b59
DannyGilligan <gilligan.danny@gmail.com>
2024-02-25 04:42:31 +0000

    Create contexts.py in cart app

---
+++ cart/contexts.py
@@ -0,0 +1,11 @@
+# Handles the cart_items variable
+
+
+def cart_contents(request):
+    """
+    This is a contexts processor to make the 'context' dictionary available across the entire application
+    """
+
+    context = {}
+
+    return context
