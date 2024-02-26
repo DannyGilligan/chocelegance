d15b007ee969ed345400794a51a7d0b45153f99f
DannyGilligan <gilligan.danny@gmail.com>
2024-02-26 18:35:29 +0000

    Add Order model to models.py in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False) # Order no. is unique and permanent
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True) # Optional
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True) # Optional
+    date = models.DateTimeField(auto_now_add=True) # Automatically sets order date and time when order is created
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0) # Calculated when order is saved
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0) # Calculated when order is saved
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0) # Calculated when order is saved
+
