cbbde85d1f4a2e89aba1ed751e1187b2da297ed8
DannyGilligan <gilligan.danny@gmail.com>
2024-02-26 06:03:44 +0000

    Add adjust_cart view to cart app views.py

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -36,3 +36,32 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart # Creates session variable and overwrites the 'cart' variable with the updated value
     return redirect(redirect_url) # Redirects user once process is complete
+
+
+
+
+
+def adjust_cart(request, item_id):
+    """ This view will adjust the quantity of the products in the cart and the related amount when the update button is clicked """
+
+    quantity = int(request.POST.get('quantity')) # Converts form quantity to integer and sends to server
+    size = None # Product size is initialised with a None value
+    if 'product_size' in request.POST: # If Product size is included in the POST request, the size variable will be updated to that value
+        size = request.POST['product_size']
+    
+    cart = request.session.get('cart', {}) # Adds 'cart' to session, or creates it if it doesn't, which will allow the user to keep adding items to cart while browsing site, session will persist until user closes browser
+  
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+        else:
+            del cart[item_id]['items_by_size'][size]
+
+    else: # If there is no size for the product, this block of code will run
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            del cart.pop[item]
+
+    request.session['cart'] = cart # Creates session variable and overwrites the 'cart' variable with the updated value
+    return redirect(reverse(view_cart)) # Redirects user back to same page once process is complete
