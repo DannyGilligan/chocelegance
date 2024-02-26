2d0a9378c924eb1048db2edaa565eb866c6c49b1
DannyGilligan <gilligan.danny@gmail.com>
2024-02-26 18:44:17 +0000

    Add OrderLineItems model to models.py in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,8 +1,13 @@
-from django.db import models
+import uuid
 
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from producs.models import Product
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False) # Order no. is unique and permanent
+    order_number = models.CharField(max_length=32, null=False, editable=False) # Order no. is unique and permanent (editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -16,4 +21,12 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0) # Calculated when order is saved
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0) # Calculated when order is saved
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0) # Calculated when order is saved
+
+
+class OrderLineItem(models.Model): # Individual shopping cart item
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems') # Order ID
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE) # Product ID
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    quantity = models.IntegerField(null=Flase, blank=False, default=0) # Required
+    lineitem_total = models.DecimalField(max_digit=6, decimal_places=2, null=False, blank=False, editable=False)
 
